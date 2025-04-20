482b8da16abf7dcda011f791cd081e8c90b1da61
Jess <122939887+jessLryan@users.noreply.github.com>
2025-04-20 10:59:55 +0100

    changed onetoone to foreignkey

--- testwebsite/solveclimate/models.py
+++ testwebsite/solveclimate/models.py
@@ -7,7 +7,7 @@ class User(models.Model):
     is_admin = models.BooleanField(default=False)
 
 class Basic_User_Details(models.Model):
-    user_id = models.OneToOneField(to=User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     date_joined = models.DateTimeField(auto_now=True)
     is_active = models.BooleanField(default=True)
@@ -18,9 +18,9 @@ class Problem(models.Model):
 
 
 class Team(models.Model):
-    problem_id = models.ForeignKey(Problem, on_delete=models.CASCADE)
+    problem = models.ForeignKey(Problem, on_delete=models.CASCADE)
 
 
 class Participant(models.Model):
-    user_id = models.ManyToOneRel(to=User, field='id', field_name='user id')
-    team_id = models.ForeignKey(to=Team, to_field='id', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
