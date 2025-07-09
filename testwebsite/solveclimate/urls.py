5dc7b8b2f152dc8e59f6dd6039c4b7a912eb58f8
Jess <122939887+jessLryan@users.noreply.github.com>
2025-07-09 17:13:15 +0100

    basic login functionality - requires creation of superuser by admin.

--- testwebsite/solveclimate/urls.py
+++ testwebsite/solveclimate/urls.py
@@ -1,7 +1,6 @@
-from django.urls import path
-
-from . import views
+from django.urls import path, include
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path("home", TemplateView.as_view(template_name="home.html"), include('django.contrib.auth.urls'), name="home"),
 ]
