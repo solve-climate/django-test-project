20f887bcac7a270036f76a50a7e6aa4502a2ee87
jessicaryan <jess.ryan@posteo.uk>
2025-11-10 13:39:47 +0000

    put signupview back

--- testwebsite/solveclimate/urls.py
+++ testwebsite/solveclimate/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 
+from .views import SignUpView
+
 urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
     path("accounts/signup/", SignUpView.as_view(), name="signup"),
