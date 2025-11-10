34d2a63caae290b190b09173b1fb6c2d60e2c2dd
jessicaryan <jess.ryan@posteo.uk>
2025-11-10 13:37:10 +0000

    remove accounts folder

--- testwebsite/solveclimate/urls.py
+++ testwebsite/solveclimate/urls.py
@@ -1,4 +1,6 @@
+from django.urls import path, include
 
 urlpatterns = [
-
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/signup/", SignUpView.as_view(), name="signup"),
 ]
--- testwebsite/testwebsite/urls.py
+++ testwebsite/testwebsite/urls.py
@@ -22,7 +22,5 @@ urlpatterns = [
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
     path('solveclimate/', include('solveclimate.urls')),
     path('admin/', admin.site.urls),
-    path('accounts/', include("accounts.urls")),
-    path("accounts/", include("django.contrib.auth.urls")),
 
 ]
